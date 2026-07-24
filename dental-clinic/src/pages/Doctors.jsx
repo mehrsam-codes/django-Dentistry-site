be3d26d99835508dac3ad02699218591b6c0029e
mehrsam <mehrsamdevelop@gmail.com>
2026-07-24 16:05:37 +0330

    Create Doctors.jsx

---
+++ dental-clinic/src/pages/Doctors.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import DoctorsSection from "../components/Doctors";
+import Footer from "../components/Footer";
+
+function Doctors() {
+  return (
+    <>
+      <Navbar />
+      <DoctorsSection />
+      <Footer />
+    </>
+  );
+}
+
+export default Doctors;
