e98c88db51be93e6ddd896801c41b651f158f749
mehrsam <mehrsamdevelop@gmail.com>
2026-07-24 15:58:25 +0330

    adding app.jsx

---
+++ dental-clinic/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Doctors from "./pages/Doctors";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/doctors" element={<Doctors />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
+}
+
+export default App;
