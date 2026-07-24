91a57db3c0835dab6be93323fef2940f04b51e70
mehrsam <mehrsamdevelop@gmail.com>
2026-07-24 16:05:34 +0330

    Create Contact.jsx

---
+++ dental-clinic/src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import ContactSection from "../components/Contact";
+import Footer from "../components/Footer";
+
+function Contact() {
+  return (
+    <>
+      <Navbar />
+      <ContactSection />
+      <Footer />
+    </>
+  );
+}
+
+export default Contact;
