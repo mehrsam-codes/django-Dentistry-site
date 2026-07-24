79d586ddb4a4e0707783a00226a1a47e277cad1e
mehrsam <mehrsamdevelop@gmail.com>
2026-07-24 16:05:43 +0330

    Create Services.jsx

---
+++ dental-clinic/src/pages/Services.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import ServicesSection from "../components/Services";
+import Footer from "../components/Footer";
+
+function Services() {
+  return (
+    <>
+      <Navbar />
+      <ServicesSection />
+      <Footer />
+    </>
+  );
+}
+
+export default Services;
